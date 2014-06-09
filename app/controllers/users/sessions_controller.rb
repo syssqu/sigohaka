1889dca7d422cfce036466abf9ee0c6d37cff5fa
njsekay <njsekay@gmail.com>
2014-06-10 08:58:20 +0900

    勤務状況報告書の印刷イメージを修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,7 +8,8 @@ class Users::SessionsController < Devise::SessionsController
   end
  
   def destroy
-    session[:user_id] = nil
-    redirect_to root_url
+    super
+    # session[:user_id] = nil
+    # redirect_to root_url
   end
 end
