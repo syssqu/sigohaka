e9fd0a1b9d0568b4093d3bbf116505c7c7acf678
njsekay <njsekay@gmail.com>
2014-06-10 18:15:15 +0900

    modify minor upgrade

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -5,7 +5,7 @@ class LicensesController < ApplicationController
   # GET /licenses
   # GET /licenses.json
   def index
-    @licenses = License.all
+    @licenses = current_user.licenses.all
   end
 
   # GET /licenses/1
