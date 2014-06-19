4ef3007ff17ff4095bb745d17ade95da60b56b3e
njsekay <njsekay@gmail.com>
2014-06-19 13:52:12 +0900

    帳票に出力するフォントを変更する,

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,5 +36,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-  config.serve_static_assets = false
+  # config.serve_static_assets = false
 end
