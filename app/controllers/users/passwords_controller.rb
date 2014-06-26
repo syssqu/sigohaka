7d6dd17a014d7b38a0fca1d40f3a0ab3968584be
njsekay <njsekay@gmail.com>
2014-06-26 14:38:13 +0900

    ログイン機能改善

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -15,24 +15,24 @@ class Users::PasswordsController < Devise::PasswordsController
   end
 
   def update
-    @user = current_user
+    # @user = current_user
  
-    # Devise::Models::DatabaseAuthenticatable#update_with_password
-    # Update record attributes when :current_password matches, otherwise returns error on :current_password.
-    # It also automatically rejects :password and :password_confirmation if they are blank.
-    if @user.update_with_password(params[:user])
+    # # Devise::Models::DatabaseAuthenticatable#update_with_password
+    # # Update record attributes when :current_password matches, otherwise returns error on :current_password.
+    # # It also automatically rejects :password and :password_confirmation if they are blank.
+    # if @user.update_with_password(params[:user])
       
-      # Sign in the user bypassing validation in case his password changed
-      sign_in @user, :bypass => true
+    #   # Sign in the user bypassing validation in case his password changed
+    #   sign_in @user, :bypass => true
       
-      redirect_to root_path, :notice => "Your Password has been updated!"
-    else
+    #   redirect_to root_path, :notice => "Your Password has been updated!"
+    # else
  
-      flash[:alert] = @user.errors.full_messages.join("<br />")
-      render :edit
+    #   flash[:alert] = @user.errors.full_messages.join("<br />")
+    #   render :edit
  
-    end
-    # super
+    # end
+    super
   end
 
   
