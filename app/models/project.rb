17a9967c6a0249008b9a382294ca461ddeb1a42b
gawa <i-togawa@sys-square.co.jp>
2016-05-13 14:47:55 +0900

    日付調整

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,11 @@ class Project < ActiveRecord::Base
 
   validates :summary, presence: true, length: { maximum: 40}
   validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :start_end_check
+
+  def start_end_check
+    errors[:base] << "開始月もしくは終了月の日付が正しくありません。"  if
+    self.start_date > self.end_date
+  end
 end
