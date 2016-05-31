1e69887e2693a4360601c7a46e79b2b02be44a19
gawa <i-togawa@sys-square.co.jp>
2016-05-31 09:17:23 +0900

    #132 住宅手当の契約期間調整

--- app/models/housing_allowance.rb
+++ app/models/housing_allowance.rb
@@ -1,3 +1,13 @@
 class HousingAllowance < ActiveRecord::Base
 	belongs_to :user
+
+	validate :start_end_check
+
+	def start_end_check
+
+		if !self.agree_date_e.blank? && self.agree_date_s > self.agree_date_e
+			errors[:base] << "正しい数値を入力してください"
+		end
+	end
+
 end
